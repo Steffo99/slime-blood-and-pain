147f063d3f307a64b5b3db54ae160a6453dbc640
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2019-04-27 19:54:06 +0200

    Add monster awareness
    Now monsters should be aware of the player if he's in 3 tiles from them

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -14,27 +14,45 @@ public class Monster : MonoBehaviour
     public Sprite sprite;
 
     private Map map;
+
+    private Player player;
     
     // Start is called before the first frame update
     private void Start()
     {
         map = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         hasSpottedPlayer = false;
     }
     
     void Turn(){
-        int direction = Random.Range(0, 4);
-        if (direction == 0 && map.CanMoveTo(Vector2Int.left)){
-            transform.Translate(Vector3.left);
+        IsPlayerNear();
+        if (!hasSpottedPlayer){
+            int direction = Random.Range(0, 4);
+            if (direction == 0 && map.CanMoveTo(Vector2Int.left)){
+                transform.Translate(Vector3.left);
+            }
+            else if (direction == 1 && map.CanMoveTo(Vector2Int.right)){
+                transform.Translate(Vector3.right);
+            }
+            else if (direction == 2 && map.CanMoveTo(Vector2Int.up)){
+                transform.Translate(Vector3.up);
+            }
+            else if (direction == 3 && map.CanMoveTo(Vector2Int.left)){
+                transform.Translate(Vector3.left);
+            }
         }
-        else if (direction == 1 && map.CanMoveTo(Vector2Int.right)){
-            transform.Translate(Vector3.right);
+        else{
+            //Put here pathfinding code
         }
-        else if (direction == 2 && map.CanMoveTo(Vector2Int.up)){
-            transform.Translate(Vector3.up);
+    }
+
+    void IsPlayerNear(){
+        if ((player.transform.position.x-transform.position.x)<=3 || (transform.position.x-player.transform.position.x)>=-3){
+            hasSpottedPlayer=true;
         }
-        else if (direction == 3 && map.CanMoveTo(Vector2Int.left)){
-            transform.Translate(Vector3.left);
+        if ((player.transform.position.y-transform.position.y)<=3 || (transform.position.y-player.transform.position.y)>=-3){
+            hasSpottedPlayer=true;
         }
     }
 }
