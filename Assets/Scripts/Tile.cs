94ff3d73d65391598e80f2de551a495c776da963
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-04-28 00:20:01 +0200

    Fix merge stuff?

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool walkable;
+    public bool roomPart;
+    public Sprite sprite;
+    
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+    }
+}
