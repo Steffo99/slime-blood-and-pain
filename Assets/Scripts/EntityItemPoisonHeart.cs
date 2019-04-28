ae322dfa9a0dc0f7321c46a5a657b5e972651558
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2019-04-28 17:30:43 +0200

    Add poisoned Heart object

---
+++ Assets/Scripts/EntityItemPoisonHeart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityItemPoisonHeart : EntityItem
+{
+    public int damage;
+    protected EntityPlayer player;
+    public override string Name {
+        get {
+            return "Poisonous Heart";
+        }
+    } 
+    // Start is called before the first frame update
+    protected new void Start() {
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<EntityPlayer>();
+    }
+
+    // Update is called once per frame
+    public override void OnPickup(EntityPlayer player) {
+        messageBar.Write("You used: " + Name, Color.yellow);
+        player.hp-=damage;
+        if (player.hp <= 0) player.Die();
+        Destroy(gameObject);
+    }
+}
